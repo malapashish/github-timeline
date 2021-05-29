5680840453617467fdc8194fb2198dacf142f1e9
malapashish <malapashish10@gmail.com>
2021-05-29 23:02:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React , {useState , useEffect} from 'react';
-import axios from 'axios';
-import { Redirect, Route } from 'react-router';
+import React  from 'react';
+import { Route } from 'react-router';
 
 
 import Login from './Pages/Login';
