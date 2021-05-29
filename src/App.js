3a234561f29422efff594b0928f07bdc2633a3f3
malapashish <malapashish10@gmail.com>
2021-05-29 23:58:45 +0530

    Error field added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React  from 'react';
 import { Route } from 'react-router';
 
 
-import Login from './Pages/Login';
-import TimeLine from './Pages/TimeLine';
+import TimeLine from './Pages/TimeLine'; 
 
 const App = () => {
     return(
         <>
-        <Route exact path = '/' component = {() => <Login />} />
-        <Route path = '/timeline'  render = {(props) => <TimeLine {...props} />} />
+        <Route exact path = '/' component = {() => <TimeLine />} /> 
         </>
     )
 }
